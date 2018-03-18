c87cf49973c6fb1b5489f4f7fd7d514543bca03a
Nikita Mishchanka <niklive103@gmail.com>
2018-03-19 00:52:32 +0300

    SMS Function Refactor
    - Tested version, it works;
    - Add initGsm and deinitGsm methods;
    - TO DO: If master does not know gsm baud rate, find it by brute force.

--- GSM/simple_sms/simple_sms.ino
+++ GSM/simple_sms/simple_sms.ino
@@ -1,49 +1,62 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial gsmSerial(2, 3); // RX, TX
+
+void initGsm(int baudRate) {
+  gsmSerial.begin(baudRate);
+  delay(1000);
+  gsmSerial.println("AT+IPR=" + String(baudRate));  //Set baud rate in GSM Module
+  delay(1000);
+  gsmSerial.println("AT"); //Check module status
+  delay(1000);
+  gsmSerial.println("AT+CMGF=1"); //Select SMS message format 
+  delay(1000);
+  gsmSerial.println("AT+CSCS=\"GSM\""); //Select TE Character Set
+  delay(1000);
+  gsmSerial.print((char)27);
+  delay(1000);
+}
+
+void deinitGsm() {
+  Serial.println("deinit gsm");
+  gsmSerial.println("AT+IPR=0");  //Set auto baud rate in GSM Module
+  delay(1000);
+}
 
 void setup() {
-  Serial.begin(1200);  //Скорость порта для связи Arduino с компьютером
+  Serial.begin(9600);  //Set baud rate in debug connection with PC
   Serial.println("Start!");
-  mySerial.begin(115200);  //Скорость порта для связи Arduino с GSM модулем
-  mySerial.println("AT+IPR=1200");
-  mySerial.begin(1200);
-  delay(1000);
-  mySerial.println("AT");
-  delay(1000);
-  mySerial.println("AT+CMGF=1");
-  delay(1000);
-  mySerial.println("AT+CSCS=\"GSM\"");
-  delay(1000);
-  mySerial.print((char)27);
+  initGsm(9600);
 }
 
 void sms(String text, String phone) {
   Serial.println("SMS send started");
-  mySerial.println("AT+CMGS=\"" + phone + "\"");
+  gsmSerial.println("AT+CMGS=\"" + phone + "\"");
   delay(1000);
-  mySerial.print(text);
+  gsmSerial.print(text);
   delay(1000);
-  mySerial.print((char)26);
+  gsmSerial.print((char)26);
   delay(1000);
   Serial.println("SMS send finish");
   delay(3000);
 }
 
-void loop() {    
+void loop() { 
     long k = 0;
     while (k < 1000000) {
-      if (mySerial.available())
-        Serial.write(mySerial.read());
+      if (gsmSerial.available())
+        Serial.write(gsmSerial.read());
       if (Serial.available())
-        mySerial.write(Serial.read());
+        gsmSerial.write(Serial.read());
       k++;
     }
-  sms(String("Hey Mottafuccka, it is me, BulbaSat!"),String("+375296289065"));
+
+  sms(String("BulbaSat revival! 9600 baud rate. More more symbols, azaza"), String("+375291000000"));
+  deinitGsm();
 
   while (true) {
-    if (mySerial.available())
-      Serial.write(mySerial.read());
+    if (gsmSerial.available())
+      Serial.write(gsmSerial.read());
     if (Serial.available())
-      mySerial.write(Serial.read());
+      gsmSerial.write(Serial.read());
   }
 }
