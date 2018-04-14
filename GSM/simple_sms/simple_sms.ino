2b4e41a8e989035d1bae078ed283f4042f3b3098
Dzmitry Bylinovich <dzmitry.bylinovich@gmail.com>
2018-04-14 11:33:19 +0300

    Bust baud rate
    - added function to "bust" baud rate
    - added some logging
    - more logging incoming

--- GSM/simple_sms/simple_sms.ino
+++ GSM/simple_sms/simple_sms.ino
@@ -2,13 +2,28 @@
 SoftwareSerial gsmSerial(2, 3); // RX, TX
 
 void initGsm(int baudRate) {
-  gsmSerial.begin(baudRate);
-  delay(1000);
-  configGsmBaudRate(); // need this to set auto baud rate to the BAUDRATE value
-  delay(1000);
-  gsmSerial.println("AT+IPR=" + String(baudRate));  //Set baud rate in GSM Module
-  delay(1000);
-  gsmSerial.println("AT"); //Check module status
+  bool inited = false;
+  long iteration = 0;
+  long maxIteration = 5;
+
+  while (!inited && iteration < maxIteration) {
+    int oldBaudRate = bustBaudRate(); // need this to set auto baud rate to the BAUDRATE value
+    gsmSerial.begin(oldBaudRate);
+    delay(1000);
+    gsmSerial.println("AT+IPR=" + String(baudRate));  //Set baud rate in GSM Module
+    delay(1000);
+    gsmSerial.begin(baudRate);
+    delay(1000);
+    inited = checkGsmStatus(baudRate); // Check Gsm Module Status
+    delay(1000);
+    ++iteration;
+  }
+  
+  if (!inited) {
+    Serial.println("SetBaudRateError: GSM haven't answered.");
+    return;
+  }
+  
   delay(1000);
   gsmSerial.println("AT+CMGF=1"); //Select SMS message format 
   delay(1000);
@@ -46,12 +61,54 @@ void deinitGsm() {
   delay(1000);
 }
 
-const int BAUDRATE = 9600;
+bool checkGsmStatus(const int baudRate) {
+  gsmSerial.begin(baudRate);
+  delay(1000);
+
+  long maxIteration = 10;
+  
+  for (int i=0; i<maxIteration; ++i) {
+    gsmSerial.println("AT");
+    delay(1000);
+    
+    if (gsmSerial.available() > 0) {
+      String readStr = gsmSerial.readStringUntil('\n');
+      
+      if (strcmp(readStr.c_str(), "OK\r") == 0) {
+        return true;
+      }
+    }
+  }
+}
+
+int bustBaudRate() {
+  const int POSSIBLE_BAUD_RATES [] = {1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200};
+  const int NUMBER_OF_BR = 8;
+ 
+  for (int i=0; i < NUMBER_OF_BR; ++i) {
+    int current_baud_rate = POSSIBLE_BAUD_RATES[i];
+    bool ok_received = checkGsmStatus(current_baud_rate);
+    
+    Serial.println("Tried baudRate = ");
+    Serial.println(String(current_baud_rate));
+    Serial.println(", received ");
+    
+    if (ok_received) {
+      Serial.println(" found ");
+      return current_baud_rate;
+    }
+    else {
+       Serial.println("not found ");
+    }
+  }
+}
+
+const int BAUD_RATE = 9600;
 
 void setup() {
-  Serial.begin(BAUDRATE);  //Set baud rate in debug connection with PC
+  Serial.begin(BAUD_RATE);  //Set baud rate in debug connection with PC
   Serial.println("Start!");
-  initGsm(BAUDRATE);
+  initGsm(BAUD_RATE);
 }
 
 void sms(String text, String phone) {
